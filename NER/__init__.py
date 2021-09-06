0a98baff79a54d1b94ff710cd577b5e866003170
Guido_Hwang <1669138300@qq.com>
2021-09-06 10:43:28 +0800

    :new: ner model

---
+++ NER/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/2 12:16
+# @Project : opinion_detection
+# @FileName: __init__.py
+# @Version ：V 1.0
+# @Software: PyCharm
+# @Github  : https://github.com/Guido-Hwang
+# 作     者：Guido_Hwang
+# @Desc    : ==============================================
+# ======================================================
+
+data_path = '../tieba_spider/tieba_spider/spiders/data/'
+info_path = './data/'
+
+
+
